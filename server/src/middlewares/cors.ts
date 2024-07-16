518dc7d252764be51b475a43b4796ec4b21ec451
Stephen Park <spark0843@gmail.com>
2024-07-16 07:23:19 -0400

    Change header from x-access-token to Authorization

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -3,6 +3,6 @@ import cors from 'cors';
 export default cors({
   origin: `http://${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 });
