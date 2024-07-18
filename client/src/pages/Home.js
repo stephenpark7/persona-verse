1c5fdf15ec628be0cc3e895344c712a3fea6ce21
Stephen Park <spark0843@gmail.com>
2024-07-17 21:39:15 -0400

    chore: Remove unused imports and code in TweetContainer.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -25,9 +25,8 @@ export default function Home() {
             </>
           ) : (
             <>
-              <TweetContainer />
               <p>Welcome {userData?.payload?.username}!</p>
-              <br />
+              <TweetContainer />
              <LogoutButton />
             </>
           )}
