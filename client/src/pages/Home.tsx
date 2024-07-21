4885592c7b71ba2dbbc77fc4ce4dee7510813503
Stephen Park <spark0843@gmail.com>
2024-07-21 02:42:25 -0400

    refactor: Improve Home page rendering logic

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,28 +8,36 @@ import LogoutButton from '../components/LogoutButton';
 const Home: React.FC = () => {
   const { userData, isLoggedIn } = useUserContext();
 
+  function renderBodyContent() {
+    if (!isLoggedIn) {
+      return (
+        <>
+          <p>Create an account or log in.</p>
+          <Link to='/signup'>
+            <Button variant="primary">Sign up</Button>
+          </Link>{' '}
+          <Link to='/login'>
+            <Button variant="primary">Log in</Button>
+          </Link>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <p>Welcome {userData?.payload?.username}!</p>
+        <TweetContainer />
+        <LogoutButton />
+      </>
+    );
+  }
+
   return (
     <Container>
       <Row className='my-5'>
         <Col>
           <h1>Twitter</h1>
-          {!isLoggedIn ? (
-            <>
-              <p>Create an account or log in.</p>
-              <Link to='/signup'>
-                <Button variant="primary">Sign up</Button>
-              </Link>{' '}
-              <Link to='/login'>
-                <Button variant="primary">Log in</Button>
-              </Link>
-            </>
-          ) : (
-            <>
-              <p>Welcome {userData?.payload?.username}!</p>
-              <TweetContainer />
-             <LogoutButton />
-            </>
-          )}
+          {renderBodyContent()}
         </Col>
       </Row>
     </Container>
