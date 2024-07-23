ad96eff3aff4372f219702c5f9c84d8388fb3df7
Stephen Park <spark0843@gmail.com>
2024-07-22 20:40:22 -0400

    Update environment variables for development to use IP address instead of localhost

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -1,7 +1,10 @@
 import cors from 'cors';
 
 export default cors({
-  origin: `http://${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
+  origin: [
+    `http://${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
+    `http://192.168.2.201:${process.env.CLIENT_PORT}`,
+  ],
   methods: [ 'GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD' ],
   allowedHeaders: [ 'Content-Type', 'Authorization' ],
   credentials: true,
