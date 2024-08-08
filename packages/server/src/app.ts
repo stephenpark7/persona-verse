d795a5f2eb721bf1a5473417d329c8fa43b7ecea
Stephen Park <spark0843@gmail.com>
2024-08-08 16:45:25 -0400

    refactor

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,14 +1,11 @@
 import { loadEnvironmentVariables } from './utils/env';
-
 loadEnvironmentVariables();
 
 import express from 'express';
+
 import { setupBindings } from './middleware';
 
 export const app = express();
-
 setupBindings(app);
-
 app.setupMiddleware();
-
 app.startServer();
