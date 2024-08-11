cfad703262051590801fb8ad199384419680b846
Stephen Park <spark0843@gmail.com>
2024-08-11 07:22:28 -0400

    fix for tsc import and output

--- packages/server/src/utils/env.ts
+++ packages/server/src/utils/env.ts
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
 
 export const loadEnvironmentVariables = () => {
-  if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'test') {
-    return;
-  }
+  const NODE_ENV = process.env.NODE_ENV || 'development';
+
+  if (NODE_ENV !== 'development' && NODE_ENV !== 'test') return;
+  
   dotenv.config({
-    path: `.env.${process.env.NODE_ENV}`,
+    path: `.env.${NODE_ENV}`,
     debug: true,
   });
 };
