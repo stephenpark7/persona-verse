108c1dc4a82d01a9e71124d6da78e5ee5ec8ec07
Stephen Park <spark0843@gmail.com>
2024-08-11 23:26:13 -0400

    build: add vite-plugin-checker

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [ react() ],
-  plugins: [ react(), tsconfigPaths() ],
+  plugins: [ 
+    react(), 
+    tsconfigPaths(),
+    checker({ typescript: true }),
+  ],
   server: {
     port: 3000,
   },
