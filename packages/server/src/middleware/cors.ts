2bcb67df69bfcdcf2d2fab386ae767929068238d
Stephen Park <spark0843@gmail.com>
2024-08-21 01:50:39 -0400

    add SERVER_HOST env

--- packages/server/src/middleware/cors.ts
+++ packages/server/src/middleware/cors.ts
@@ -3,8 +3,6 @@ import cors from 'cors';
 export const corsMiddleware = () => cors({
   origin: [
     `http://${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
-    `https://${process.env.CLIENT_HOST}:3001`,
-    `https://${process.env.CLIENT_HOST}:3002`,
   ],
   methods: [ 'GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD' ],
   allowedHeaders: [ 'Content-Type', 'Authorization' ],
