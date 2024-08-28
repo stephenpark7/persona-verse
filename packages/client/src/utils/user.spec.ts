00e58bd35332a5d68804163242212ee53ae46a79
Stephen Park <spark0843@gmail.com>
2024-08-28 02:22:11 -0400

    minor refactor

--- packages/client/src/utils/user.spec.ts
+++ packages/client/src/utils/user.spec.ts
@@ -4,7 +4,6 @@ import { getDisplayName } from './user';
 describe('getDisplayName', () => {
   test('returns the username from the JWT payload', () => {
     const jwt = jwtFactory();
-    const result = getDisplayName(jwt);
-    expect(result).toBe(jwt.payload.username);
+    expect(getDisplayName(jwt)).toBe(jwt.payload.username);
   });
 });
