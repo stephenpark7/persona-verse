1971932c96a36b3873631a0c5c27b9b2cbf3f497
Stephen Park <spark0843@gmail.com>
2024-08-28 06:26:40 -0400

    reword test

--- packages/client/src/pages/Home/components/Header.unit.spec.tsx
+++ packages/client/src/pages/Home/components/Header.unit.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { Header } from '@pages';
 
 describe('Header Component', () => {
-  test('renders the Header component', () => {
+  test('renders the title', () => {
     render(<Header title={'PersonaVerse'} />);
     expect(screen.getByRole('heading', { name: /PersonaVerse/, level: 1 })).toBeInTheDocument();
   });
