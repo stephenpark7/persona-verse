0a396f0fc2bb192b362970799fe8b09837805af8
Stephen Park <spark0843@gmail.com>
2024-08-28 06:26:59 -0400

    lint

--- packages/client/src/pages/Home/components/Header.unit.spec.tsx
+++ packages/client/src/pages/Home/components/Header.unit.spec.tsx
@@ -4,6 +4,9 @@ import { Header } from '@pages';
 describe('Header Component', () => {
   test('renders the title', () => {
     render(<Header title={'PersonaVerse'} />);
-    expect(screen.getByRole('heading', { name: /PersonaVerse/, level: 1 })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { 
+      name: /PersonaVerse/, 
+      level: 1,
+    })).toBeInTheDocument();
   });
 });
