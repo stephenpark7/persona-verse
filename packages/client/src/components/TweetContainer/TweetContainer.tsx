dd34b1fc09ac89c2661375befec2ab5596bdb15a
Stephen Park <spark0843@gmail.com>
2024-08-29 19:28:28 -0400

    add tweetbutton

--- packages/client/src/components/TweetContainer/TweetContainer.tsx
+++ packages/client/src/components/TweetContainer/TweetContainer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { TweetInput } from './components/TweetInput';
 import { Tweets } from './components/Tweets';
+import { TweetButton } from './components/TweetButton';
 
 export const TweetContainer: React.FC = (): React.JSX.Element => {
   return (
     <div>
       <TweetInput />
+      <TweetButton />
       <Tweets  />
     </div>
   );
