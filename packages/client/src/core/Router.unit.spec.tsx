b9fb42ca7e74f3bca7c1b53aea05426889a3cb16
Stephen Park <spark0843@gmail.com>
2024-09-04 20:26:11 -0400

    wip test

--- packages/client/src/core/Router.unit.spec.tsx
+++ packages/client/src/core/Router.unit.spec.tsx
@@ -1,1 +1,42 @@
-// TODO: mock the router and test the routes
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { createMemoryRouter } from 'react-router-dom';
+import { Home, Login, Signup } from '@pages';
+import { vi } from 'vitest';
+import { screen } from '@testing-library/react';
+import { renderApp } from 'src/tests/utils';
+import { MainLayout } from 'src/layouts';
+
+vi.mock('src/layouts/main', () => ({
+  MainLayout: ({
+    title,
+    children,
+  }: {
+    title: string;
+    children: React.ReactNode;
+  }) => (
+    <div>
+      <h1>{title}</h1>
+      {children}
+    </div>
+  ),
+}));
+
+describe('Router', () => {
+  const routes = [
+    { path: '/', element: <Home /> },
+    { path: '/login', element: <Login /> },
+    { path: '/signup', element: <Signup /> },
+  ];
+
+  it.only('renders title', () => {
+    const router = createMemoryRouter(routes, { initialEntries: ['/'] });
+    const children = <RouterProvider router={router} />;
+    renderApp(<MainLayout title="PersonaVerse">{children}</MainLayout>);
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'PersonaVerse',
+    );
+  });
+
+  // TODO: check to make sure MainLayout and RouterProvider are returned
+});
