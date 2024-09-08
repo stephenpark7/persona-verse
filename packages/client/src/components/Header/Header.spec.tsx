029a203c25793d608d157741e94601b44c662d18
Stephen Park <spark0843@gmail.com>
2024-09-08 07:23:16 -0400

    refactor

--- packages/client/src/components/Header/Header.spec.tsx
+++ packages/client/src/components/Header/Header.spec.tsx
@@ -2,9 +2,11 @@ import { render, screen } from '@testing-library/react';
 import { Header } from '@pages';
 
 describe('Header Component', () => {
+  const title = 'PersonaVerse';
+
   test('renders the title', () => {
-    const title = 'PersonaVerse';
     render(<Header title={title} />);
+
     expect(
       screen.getByRole('heading', {
         name: title,
