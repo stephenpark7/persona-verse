66a3d42359ed803a760eea686375202f32de0b74
Stephen Park <spark0843@gmail.com>
2024-09-08 04:09:41 -0400

    rm unused

--- packages/client/src/schemas/api.ts
+++ packages/client/src/schemas/api.ts
@@ -1,21 +1,6 @@
 import { z } from 'zod';
-// import {
-//   RawAxiosRequestConfigSchema,
-//   RawAxiosRequestHeadersSchema,
-// } from './axios';
 import { navigateFunction } from './form';
 import { requestBody } from './request';
-
-// export const apiProtocol = z.enum(['rest', 'trpc']);
-
-// export const ApiCallSchema = z.object({
-//   method: z.string(),
-//   controller: z.string(),
-//   action: z.string(),
-//   body: requestBody.optional(),
-//   options: RawAxiosRequestConfigSchema.optional(),
-//   headers: RawAxiosRequestHeadersSchema.optional(),
-// });
 
 export const ApiFunctionSchema = z
   .function()
