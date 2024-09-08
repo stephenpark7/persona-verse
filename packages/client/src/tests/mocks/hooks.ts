66d2e5305cdd5144c6e82600252458ab685bf1aa
Stephen Park <spark0843@gmail.com>
2024-09-08 07:06:57 -0400

    rm comment

--- packages/client/src/tests/mocks/hooks.ts
+++ packages/client/src/tests/mocks/hooks.ts
@@ -2,17 +2,6 @@ import { vi } from 'vitest';
 import { useUserState } from '@hooks';
 import { UserType, useUserStateFactory } from '@factories';
 
-// TODO:
-// Should only be used in unit tests
-// so we should use renderApp instead of renderPage
-// to wrap the component in a Router with providers
-// and preloaded state (need to figure out tweetsAPI default state)
-// also remove import from ./index.ts
-// and directly import this file wherever needed
-// in the unit spec files only
-// we should give preloaded state a default value for useUserState
-// and pass it to renderApp
-
 vi.mock('@hooks', () => ({
   useUserState: vi.fn(),
 }));
