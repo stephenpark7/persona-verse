eff5f4175f57ea5bc1fdc85aef1f744fc273a28e
Stephen Park <spark0843@gmail.com>
2024-09-08 07:05:02 -0400

    add test for welcome message

--- packages/client/src/pages/Home/components/WelcomeMessage.spec.tsx
+++ packages/client/src/pages/Home/components/WelcomeMessage.spec.tsx
@@ -5,7 +5,7 @@ import { UserType, useUserStateFactory } from '@factories';
 import { WelcomeMessage } from '@pages';
 
 describe('WelcomeMessage Component', () => {
-  describe('when logged out', () => {
+  describe('while logged out', () => {
     test('renders the welcome message', () => {
       useUserStateStub(UserType.GUEST);
       const useUserState = () => useUserStateFactory(UserType.GUEST);
@@ -14,6 +14,16 @@ describe('WelcomeMessage Component', () => {
       expect(
         screen.getByText('Create an account or log in.'),
       ).toBeInTheDocument();
+    });
+  });
+
+  describe('while logged in', () => {
+    test('renders the welcome message', () => {
+      useUserStateStub(UserType.USER);
+      const useUserState = () => useUserStateFactory(UserType.USER);
+      const { jwt, isLoggedIn } = useUserState();
+      render(<WelcomeMessage jwt={jwt} isLoggedIn={isLoggedIn} />);
+      expect(screen.getByText('Welcome john-doe!')).toBeInTheDocument();
     });
   });
 });
