1d2e895c2a67fa8b051a1068a8459bb90ee84bc7
Stephen Park <spark0843@gmail.com>
2024-09-22 04:57:19 -0400

    Start working on private/public routes functionality

--- packages/client/src/core/Router.tsx
+++ packages/client/src/core/Router.tsx
@@ -1,8 +1,9 @@
+import { FC } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from '@pages';
 
 export const router = createBrowserRouter(routes);
 
-export const Router: React.FC = () => {
+export const Router: FC = () => {
   return <RouterProvider router={router} />;
 };
--- packages/client/src/pages/routes.tsx
+++ packages/client/src/pages/routes.tsx
@@ -1,10 +1,8 @@
+import { FC, ReactNode } from 'react';
 import { MainLayout } from '@layouts';
 import { Home, Login, Signup, Dashboard, Profile } from '@pages';
 import { Route, routeSchema } from '@schemas';
 import { Navbar } from '@components';
-
-// TODO: add public and private routes
-// for Dashboard and Profile
 
 const layoutWrapper = (
   element: React.ReactNode,
@@ -17,37 +15,62 @@ const layoutWrapper = (
   </>
 );
 
+interface RouteProps {
+  element: ReactNode;
+}
+
+const PrivateRoute: FC<RouteProps> = ({ element }) => {
+  return <>{element}</>;
+};
+
+const PublicRoute: FC<RouteProps> = ({ element }) => {
+  return <>{element}</>;
+};
+
 export const routes: Route[] = [
   {
     path: '/signup',
     element: <Signup />,
     title: 'PersonaVerse - Sign up',
     hideNavbar: true,
+    private: false,
   },
   {
     path: '/login',
     element: <Login />,
     title: 'PersonaVerse - Log in',
     hideNavbar: true,
+    private: false,
   },
   {
     path: '/dashboard',
     element: <Dashboard />,
     title: 'PersonaVerse - Dashboard',
+    private: true,
   },
   {
     path: '/profile',
     element: <Profile />,
     title: 'PersonaVerse - Profile',
+    private: true,
   },
   {
     path: '/',
     element: <Home />,
     title: 'PersonaVerse',
     hideNavbar: true,
+    private: false,
   },
 ].map((route) => {
-  route.element = layoutWrapper(route.element, route.title, route.hideNavbar);
+  route.element = layoutWrapper(
+    route.private ? (
+      <PrivateRoute element={route.element} />
+    ) : (
+      <PublicRoute element={route.element} />
+    ),
+    route.title,
+    route.hideNavbar,
+  );
   return route;
 });
 
