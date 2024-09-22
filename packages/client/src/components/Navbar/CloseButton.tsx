28b3eee7a6a95b42b1eb4295674940b380faaaae
Stephen Park <spark0843@gmail.com>
2024-09-22 03:22:37 -0400

    Refactor Dropdown component to use separate Item and CloseButton components

---
+++ packages/client/src/components/Navbar/CloseButton.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+interface CloseButtonProps {
+  closeBurgerMenu: () => void;
+}
+
+export const CloseButton: FC<CloseButtonProps> = ({ closeBurgerMenu }) => {
+  return (
+    <div
+      className="text-lg cursor-pointer hover:text-red-700 ease-in-out duration-200"
+      onClick={closeBurgerMenu}
+    >
+      Close
+    </div>
+  );
+};
--- packages/client/src/components/Navbar/Dropdown.tsx
+++ packages/client/src/components/Navbar/Dropdown.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 import { LogoutButton } from '@components';
+import { Item } from './Item';
+import { CloseButton } from './CloseButton';
 
 interface DropdownProps {
   isBurgerMenuOpen: boolean;
@@ -11,17 +13,12 @@ export const Dropdown: FC<DropdownProps> = ({ closeBurgerMenu }) => {
     <div className="dropdown absolute right-0 top-0 bg-white border border-gray-200 border-opacity-80 rounded-md drop-shadow-lg">
       <div className="flex flex-col min-w-24 min-h-12 justify-center p-3">
         <ul className="flex flex-col items-end justify-end gap-3">
-          <li className="text-lg text-gray-900 cursor-pointer hover:text-gray-500 ease-in-out duration-200">
+          <Item>
             <LogoutButton />
-          </li>
-          <li>
-            <div
-              className="text-lg cursor-pointer hover:text-red-700 ease-in-out duration-200"
-              onClick={closeBurgerMenu}
-            >
-              Close
-            </div>
-          </li>
+          </Item>
+          <Item>
+            <CloseButton closeBurgerMenu={closeBurgerMenu} />
+          </Item>
         </ul>
       </div>
     </div>
