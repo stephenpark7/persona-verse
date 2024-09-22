cb7dfdec08cd31faa547140fad8c2ca80bc9ec3b
Stephen Park <spark0843@gmail.com>
2024-09-21 23:15:19 -0400

    Refactor Navbar.tsx to remove unused import

--- packages/client/src/components/Navbar/Navbar.tsx
+++ packages/client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, FC } from 'react';
+import { useEffect, useState, FC } from 'react';
 import { Link } from 'react-router-dom';
 import { BurgerMenuSvg } from '../BurgerMenuSvg';
 import { Dropdown } from './Dropdown';
