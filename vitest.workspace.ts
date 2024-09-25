16e7878d20f73d3151a07d1799483e130196e07a
Stephen Park <spark0843@gmail.com>
2024-09-25 07:02:02 -0400

    fix workspace file

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,17 +1,6 @@
 import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
-  // 'packages/*',
-  {
-    extends: './packages/client/vitest.config.ts',
-    test: {
-      setupFiles: './packages/client/src/tests/vitest.setup.ts',
-    },
-  },
-  {
-    extends: './packages/server/vitest.config.ts',
-    test: {
-      setupFiles: './packages/server/src/tests/vitest.setup.ts',
-    },
-  }
+  'packages/client',
+  'packages/server',
 ]);
