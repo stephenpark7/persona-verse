b0808cd6cdaa1df71974ae43057e9c73b873a2ce
Stephen Park <spark0843@gmail.com>
2024-09-30 09:16:11 -0400

    Refactor Logo component to include data-testid attributes and update logo link

--- packages/client/src/tests/helpers/screen.ts
+++ packages/client/src/tests/helpers/screen.ts
@@ -3,4 +3,5 @@ import { screen as origScreen } from '@testing-library/react';
 export const screen = {
   debug: origScreen.debug,
   getByTestId: origScreen.getByTestId,
+  getByText: origScreen.getByText,
 };
