51fc92771af66a6bc2058108ce9ceed814919add
Stephen Park <spark0843@gmail.com>
2024-10-03 17:02:16 -0400

    rm comment

--- packages/client/src/hooks/useDocumentTitle.ts
+++ packages/client/src/hooks/useDocumentTitle.ts
@@ -1,5 +1,3 @@
-// TODO: lets use redux to store the title and update it from there
-
 import { useLayoutEffect } from 'react';
 import { store } from '@redux';
 import { setDocTitle } from '@redux';
