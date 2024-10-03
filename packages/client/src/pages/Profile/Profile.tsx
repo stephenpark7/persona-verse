777f2ccb86a9c4dc738aacb093eb571a63510b48
Stephen Park <spark0843@gmail.com>
2024-10-03 18:20:38 -0400

    complete spec for profile

--- packages/client/src/pages/Profile/Profile.tsx
+++ packages/client/src/pages/Profile/Profile.tsx
@@ -1,20 +1,35 @@
-// import { useUserState } from '@hooks';
 import type { FC } from 'react';
 
 export const Profile: FC = () => {
-  // const { jwt, isLoggedIn } = useUserState();
-
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-900">Profile</h1>
+    <div
+      data-testid="profile"
+      className="flex flex-col items-center justify-center"
+    >
+      <h1
+        aria-label="header"
+        className="text-3xl font-bold text-gray-900"
+      >
+        Profile
+      </h1>
       <div className="flex flex-col items-center justify-center mt-4">
         <img
           src="https://placehold.co/128x128"
           alt="Profile"
           className="w-24 h-24 rounded-full"
         />
-        <h2 className="text-xl font-semibold text-gray-900 mt-2">@username</h2>
-        <p className="text-lg text-gray-500 mt-1">Short description</p>
+        <h2
+          aria-label="username"
+          className="text-xl font-semibold text-gray-900 mt-2"
+        >
+          @username
+        </h2>
+        <p
+          aria-label="bio"
+          className="text-lg text-gray-500 mt-1"
+        >
+          Short description
+        </p>
       </div>
     </div>
   );
