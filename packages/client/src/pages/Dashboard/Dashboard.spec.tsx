c128b7e2c4dbfa905293b76288181bc43ab9b633
Stephen Park <spark0843@gmail.com>
2024-10-03 18:00:04 -0400

    Add spec for dashboard

---
+++ packages/client/src/pages/Dashboard/Dashboard.spec.tsx
@@ -0,0 +1,32 @@
+import { renderWithRouter, screen } from '@tests/helpers';
+import { preloadedStateFactory } from '@factories';
+import { Dashboard } from './Dashboard';
+
+describe('Dashboard', () => {
+  describe('when user is not logged in', () => {
+    beforeEach(() => {
+      const preloadedState = preloadedStateFactory({
+        jwt: null,
+        tweets: null,
+      });
+
+      renderWithRouter(<Dashboard />, preloadedState);
+    });
+
+    it('does not render the dashboard', () => {
+      expect(screen.queryByTestId('dashboard')).not.toBeInTheDocument();
+    });
+  });
+
+  describe('when user is logged in', () => {
+    beforeEach(() => {
+      const preloadedState = preloadedStateFactory();
+
+      renderWithRouter(<Dashboard />, preloadedState);
+    });
+
+    it('renders the dashboard', () => {
+      expect(screen.getByTestId('dashboard')).toBeInTheDocument();
+    });
+  });
+});
