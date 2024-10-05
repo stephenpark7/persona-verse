de459814429be4ca39b21eb16da15a0d8a5fc8e8
Stephen Park <spark0843@gmail.com>
2024-10-05 02:28:02 -0400

    Refactor imports and update trpc.ts

--- packages/client/src/trpc/trpc.ts
+++ packages/client/src/trpc/trpc.ts
@@ -9,10 +9,10 @@ import { observable } from '@trpc/server/observable';
 import type { AppRouter } from 'server/src/trpc';
 import {
   type JsonResponse,
+  type Jwt,
+  type TrpcFunction,
   registerFormFields,
   loginFormFields,
-  type Jwt,
-  TrpcFunction,
 } from '@schemas';
 import { apiConfig } from '@utils';
 import { clearJwt, setJwt, store, tweetAPI } from '@redux';
