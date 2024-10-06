bc94ca3ff80fe3c67bb2f4acf1f884904fa70314
Stephen Park <spark0843@gmail.com>
2024-10-06 04:59:59 -0400

    fix import

--- packages/shared/src/types/jwt.d.ts
+++ packages/shared/src/types/jwt.d.ts
@@ -1,5 +1,5 @@
 import type { InferType } from '.';
-import type { jwtPayload, accessToken, refreshToken } from '../src/schemas';
+import { jwtPayload, accessToken, refreshToken } from '@schemas';
 
 export type Jwt = InferType<typeof jwt>;
 
