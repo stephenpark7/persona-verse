1f81fc58c6385185e9d972e086fcb26ead0ecd14
Stephen Park <spark0843@gmail.com>
2024-10-06 04:58:30 -0400

    rm comments

--- packages/server/src/factories/refreshToken.ts
+++ packages/server/src/factories/refreshToken.ts
@@ -3,11 +3,6 @@ import { jwtPayload } from '@shared/schemas';
 import { generateRefreshToken } from '@utils';
 
 export const refreshTokenFactory = (payload: JwtPayload) => {
-  // TODO: this line breaks @shared
   jwtPayload.parse(payload);
-
-  // we need to rename jwt.d.ts to jwt.ts
-  // probably also add eslint, package.json, and tsconfig.json
-  // for shared package folder
   return generateRefreshToken(payload);
 };
