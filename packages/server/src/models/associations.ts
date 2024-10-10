cb0f628ec4399692576dc6a9f7103c9e5bc8a9d2
Stephen Park <spark0843@gmail.com>
2024-10-09 22:43:14 -0400

    rm comments

--- packages/server/src/models/associations.ts
+++ packages/server/src/models/associations.ts
@@ -4,19 +4,11 @@ import { Tweet } from './Tweet';
 import { User } from './User';
 import { UserProfile } from './UserProfile';
 
-// Refresh token
-
 RefreshToken.belongsTo(User);
-
-// Revoked token
 
 RevokedToken.belongsTo(User);
 
-// Tweet
-
 Tweet.belongsTo(User);
-
-// User
 
 User.hasMany(Tweet);
 
@@ -25,7 +17,5 @@ User.hasMany(RevokedToken);
 User.hasMany(RefreshToken);
 
 User.hasOne(UserProfile);
-
-// UserProfile
 
 UserProfile.belongsTo(User);
