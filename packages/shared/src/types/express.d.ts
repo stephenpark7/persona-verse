dd4cada819c6cdd44f20a31ac5de5654ecae3d20
Stephen Park <spark0843@gmail.com>
2024-10-21 00:04:39 -0400

    add note

--- packages/shared/src/types/express.d.ts
+++ packages/shared/src/types/express.d.ts
@@ -1,13 +1,15 @@
-import { startServer } from 'server/src/server';
-import { setupMiddleware } from 'server/src/middleware';
+import { startServer } from '../../../server/src/server';
+import { setupMiddleware } from '../../../server/src/middleware';
 import type { Jwt } from './jwt';
 
+// NOTE: can't correctly infer the type due to Zod, it will be any
 declare module 'express-session' {
   interface SessionData {
     refreshToken: Jwt;
     clearSession: () => void;
   }
 }
+
 declare module 'express-serve-static-core' {
   interface Application {
     setupMiddleware: typeof setupMiddleware;
--- packages/shared/src/types/index.d.ts
+++ packages/shared/src/types/index.d.ts
@@ -1,6 +1,6 @@
+export * from './controller';
 export * from './express';
 export * from './jwt';
 export * from './request';
 export * from './response';
-export * from './controller';
 export * from './sequelize';
