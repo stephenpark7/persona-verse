32fc2b36913214057bbd4217361f36c86ef57626
Stephen Park <spark0843@gmail.com>
2024-10-24 02:57:47 -0400

    complete spec

--- packages/server/src/controllers/refresh.spec.ts
+++ packages/server/src/controllers/refresh.spec.ts
@@ -1,4 +1,5 @@
 import type { RefreshToken, RefreshTokenResponse } from '@shared/types';
+import type { Request } from 'express';
 import {
   refreshTokenFactory,
   userFactory,
@@ -10,6 +11,7 @@ describe('Refresh Controller', async () => {
   describe('refreshJwt', async () => {
     let refreshToken: RefreshToken;
     let res: RefreshTokenResponse;
+    let req: Request;
 
     describe('when the refresh token is valid', async () => {
       beforeEach(async () => {
@@ -19,20 +21,23 @@ describe('Refresh Controller', async () => {
           userId: user.id,
           username: user.username,
         });
-      });
 
-      it.only('returns a new access token', async () => {
-        const req = requestFactory({
+        req = requestFactory({
           session: {
             refreshToken,
           },
         });
 
         res = await refreshJwt(req);
+      });
 
+      it('returns a message and jwt', async () => {
         expect(res).toHaveProperty('message', 'Token refreshed.');
         expect(res).toHaveProperty('jwt');
-        // expect(res.jwt.expiresAt).toBeGreaterThan(Date.now());
+      });
+
+      it('returns a jwt with the correct payload', async () => {
+        expect(res.jwt.payload.expiresAt).toBeDefined();
         expect(res.jwt.payload.userId).toEqual(refreshToken.payload.userId);
         expect(res.jwt.payload.username).toEqual(refreshToken.payload.username);
         expect(res.jwt.payload.jti).not.toEqual(refreshToken.payload.jti);
@@ -40,30 +45,17 @@ describe('Refresh Controller', async () => {
     });
 
     describe('when the refresh token is invalid', async () => {
-      // beforeEach(async () => {
-      // });
-
-      it('throws an error', async () => {
-        await expect(
-          async () =>
-            await refreshTokenFactory({
-              userId: -1,
-              username: 'invalid',
-            }),
-        ).rejects.toThrow('User not found.');
-
-        // await expect(() =>
-        //   refreshJwt(
-        //     requestFactory({
-        //       session: {
-        //         refreshToken,
-        //       },
-        //     }),
-        //   ),
-        // ).rejects.toThrow('User does not exist.');
+      describe('when user is not found', async () => {
+        it('throws an error', async () => {
+          await expect(
+            async () =>
+              await refreshTokenFactory({
+                userId: -1,
+                username: 'invalid',
+              }),
+          ).rejects.toThrow('User not found.');
+        });
       });
-
-      // TODO: add tests for specific error messages
     });
   });
 });
